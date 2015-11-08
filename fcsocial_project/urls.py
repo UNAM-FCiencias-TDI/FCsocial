7003d8a4553191bb51095df40a1fa77a8d0117fa
kcontrerasm <karlaaw.kc@gmail.com>
2015-11-08 15:26:03 -0600

    Registro folder templates

--- fcsocial_project/urls.py
+++ fcsocial_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from fcsocial_app import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^$', views.FC_Social_template),
+    ]
